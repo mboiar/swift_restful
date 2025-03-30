7e4e8a66c57674e2f38167a3c921ec937c288599
MAKSYM <boiar.max@gmail.com>
2025-03-30 11:33:58 +0200

    restructure database, allow for null address

--- repository/migration/000001_init_schema.up.sql
+++ repository/migration/000001_init_schema.up.sql
@@ -6,15 +6,10 @@ CREATE TABLE country (
 CREATE TABLE bank (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(200) NOT NULL,
-    address VARCHAR(200) NOT NULL,
+    address VARCHAR(200),
     swift_code CHAR(11) NOT NULL UNIQUE,
     country_ISO2 CHAR(2) NOT NULL,
-    is_headquarter BOOLEAN NOT NULL,
-    headquarter_id INT,
     FOREIGN KEY (country_ISO2)
         REFERENCES country (ISO2)
-        ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (headquarter_id)
-        REFERENCES bank (id)
-        ON DELETE SET NULL ON UPDATE CASCADE
+        ON DELETE CASCADE ON UPDATE CASCADE
 );
